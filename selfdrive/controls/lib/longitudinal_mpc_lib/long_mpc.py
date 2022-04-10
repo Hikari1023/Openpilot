ecaa53b9143ea6102059446e659edd0a4c06807b
CT921 <tim.chen242@gmail.com>
2022-04-10 19:32:39 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -350,7 +350,8 @@ class LongitudinalMpc:
   def update_TF(self, carstate):
     if carstate.distanceLines == 1: # Traffic
       self.desired_TF = 0.9
-      self.desired_stop_distance = STOP_DISTANCE - 1
+      self.desired_stop_distance = STOP_DISTANCE - 2
+
     elif carstate.distanceLines == 2: # Relaxed
       self.desired_TF = (T_FOLLOW - 1) * 0.5 + 1
       self.desired_stop_distance = STOP_DISTANCE - 0.5
