b2ea7f9c5e041286335f19a6b8457749378c8622
CT921 <tim.chen242@gmail.com>
2022-04-23 23:59:13 +0800

    Toyota-steer-faults

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -11,6 +11,9 @@ from opendbc.can.packer import CANPacker
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
+STEER_FAULT_MAX_RATE = 100
+STEER_FAULT_MAX_FRAMES = 18
+
 
 class CarController:
   def __init__(self, dbc_name, CP, VM):
@@ -56,12 +59,21 @@ class CarController:
     apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, self.torque_rate_limits)
     self.steer_rate_limited = new_steer != apply_steer
 
-    # Cut steering while we're in a known fault state (2s)
-    if not CC.latActive or CS.steer_state in (9, 25):
+    # EPS_STATUS->LKA_STATE either goes to 21 or 25 on rising edge of a steering fault and
+    # the value seems to describe how many frames the steering rate was above 100 deg/s, so
+    # cut torque with some margin for the lower state
+    if CC.latActive and abs(CS.out.steeringRateDeg) >= STEER_FAULT_MAX_RATE:
+      self.rate_limit_counter += 1
+    else:
+      # TODO: unclear if it resets its internal state at another value
+      self.rate_limit_counter = 0
+
+    apply_steer_req = 1
+    if not CC.latActive:
       apply_steer = 0
       apply_steer_req = 0
-    else:
-      apply_steer_req = 1
+    elif self.rate_limit_counter > STEER_FAULT_MAX_FRAMES:
+      apply_steer_req = 0
 
     # TODO: probably can delete this. CS.pcm_acc_status uses a different signal
     # than CS.cruiseState.enabled. confirm they're not meaningfully different
