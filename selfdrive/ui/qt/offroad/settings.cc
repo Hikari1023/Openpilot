91fd2a60944d18393a0a5943e938f012a40bae26
CT921 <tim.chen242@gmail.com>
2022-06-09 22:41:55 +0800

    update

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -191,7 +191,7 @@ void DevicePanel::updateCalibDescription() {
       if (calib.getCalStatus() != 0) {
         double pitch = calib.getRpyCalib()[1] * (180 / M_PI);
         double yaw = calib.getRpyCalib()[2] * (180 / M_PI);
-        desc += QString(" Your device is pointed %1° %2 and %3° %4.")
+        desc += QString(" 您的設備偏差 %1° %2 and %3° %4.")
                     .arg(QString::number(std::abs(pitch), 'g', 1), pitch > 0 ? "下" : "上",
                          QString::number(std::abs(yaw), 'g', 1), yaw > 0 ? "左" : "右");
       }
